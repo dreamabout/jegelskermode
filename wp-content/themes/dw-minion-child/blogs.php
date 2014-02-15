43afec056b17023db442bd852d9795152cd1dcab
Tobias Møller Kjærsgaard <tobias.kjr@gmail.com>
2014-02-15 12:33:24 +0100

    initial blog listing for debug purpose

--- wp-content/themes/dw-minion-child/blogs.php
+++ wp-content/themes/dw-minion-child/blogs.php
@@ -17,15 +17,33 @@
 
                 <?php endwhile; ?>
                     <div class="clearfix bloglist">
-                        
-                    <?php
-                    $blogs = array();
+                    	<?php // fetch blogs from autoblogs-table.
+                    	global $wpdb;
 
-                    foreach ($blogs as $blog) {
-                    	
-                    }
+                    	$table = $wpdb->prefix . 'autoblog';
+                    	$rows = $wpdb->get_col( $wpdb->prepare( "SELECT feed_meta FROM {$table}", null ), 0 );
 
-                    ?>
+                        $sorted = array();
+
+                    	foreach ($rows as $blog) {
+                    		$blog = unserialize($blog);
+                            $name = trim($blog['bloginfo']['name'], ' \t\n\r\0\x0B".*\'-');
+                            $link = $blog['bloginfo']['link'];
+                            $sorted[$name] = $link;
+                    	}
+
+                        ksort($sorted, SORT_LOCALE_STRING);
+
+                        foreach ($sorted as $name => $link) {
+                            printf('<a href="%s">%s</a><br>', $link, $name);
+                        }
+
+                        print "<pre>";
+                        print htmlspecialchars(print_r($sorted, true));
+                        print "\r\n\r\n";
+                        print "</pre>";
+
+                    	?>
                     </div>
                 </div>
             </div>
